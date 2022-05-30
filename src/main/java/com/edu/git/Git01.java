948d34715925abb9efebd06f45f7f02314216fa2
xyp <1037320558@qq.com>
2022-05-30 11:20:10 +0800

    version1

---
+++ src/main/java/com/edu/git/Git01.java
@@ -0,0 +1,16 @@
+package com.edu.git;
+
+/**
+ * @author qingwen
+ * @version 1.0
+ *
+ * 红--工作区
+ * 绿--暂存区
+ *
+ */
+public class Git01 {
+    public static void main(String[] args) {
+
+        System.out.println("hello,git!");
+    }
+}
---
+++ src/main/java/com/edu/git/Git02.java
@@ -0,0 +1,8 @@
+package com.edu.git;
+
+/**
+ * @author qingwen
+ * @version 1.0
+ */
+public class Git02 {
+}
